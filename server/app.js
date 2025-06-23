5d35df833ac460419dd53f39016a30b1710c1f97
Exabyte50 <gerardo_velasquez@dlsu.edu.ph>
2025-06-23 12:06:00 +0800

    Initial Setup
    - Node and dev dependencies installed
    - Basic React/Vite Setup
    - Tailwind not yet installed
    - MongoDB not yet connected

--- server/app.js
+++ server/app.js
@@ -1,1 +1,23 @@
-// go guyz
+require('dotenv').config()
+const express = require('express')
+const indexRoute = require('./routes/index')
+
+// express app
+const app = express()
+const PORT =  process.env.PORT || 4000
+
+// middleware
+app.use(express.json())
+app.use((req, res, next) => { // custom, for logging
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/index', indexRoute)
+
+// listen for requests
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+    console.log(`Server: http://localhost:${PORT}`)
+})
