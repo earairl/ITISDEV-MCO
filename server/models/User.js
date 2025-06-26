4b37144ac35883363c0b93e145985588f73eeac4
roms-jr <romeorasay11@gmail.com>
2025-06-26 09:54:48 +0800

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     credentials: {
-        userId:         {type: String, unique: true, default: uuidv4},
+        userId:         {type: String, required: true, unique: true},
         username:       {type: String, required: true, unique: true},
         email:          {type: String, required: true, unique: true},
         passwordSalt:   {type: String, required: function () { return !this.deleted; }},
         passwordHash:   {type: String, required: function () { return !this.deleted; }}
     },
-    penalties:          [{ type: mongoose.Schema.Types.ObjectId, ref: 'Penalty' }],
-    currentlyQueued:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'Match' }],
-    matchHistory:       [{ type: mongoose.Schema.Types.ObjectId, ref: 'Match' }],
+    // penalties:          [{ type: mongoose.Schema.Types.ObjectId, ref: 'Penalty' }],
+    // currentlyQueued:    [{ type: mongoose.Schema.Types.ObjectId, ref: 'Match' }],
+    // matchHistory:       [{ type: mongoose.Schema.Types.ObjectId, ref: 'Match' }],
     deleted:            { type: Boolean, default: false }
 }, {collection: 'Users'});
 
