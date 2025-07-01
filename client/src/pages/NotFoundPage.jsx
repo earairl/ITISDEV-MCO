4f1610909bb94e284213c943f6e9d13699c6b247
Exabyte50 <gerardo_velasquez@dlsu.edu.ph>
2025-07-01 22:31:09 +0800

    Designed NotFoundPage

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,7 +1,14 @@
+import NotFoundIcon from "../assets/PageNotFound.png";
+import styles from "./NotFoundPage.module.css";
+
+
 function NotFoundPage() {
     return (
-        <div>
-            <h1 className='text-white'>PAGE NOT FOUND</h1>
+        <div className={styles.MainDiv}>
+            <img src={NotFoundIcon} alt="Page Not Found" />
+            <h1>PAGE NOT FOUND</h1>
+            <h2> Sorry, but it seems this page does not exist. Kindly check if the URL is correct.</h2>
+            <h2>- The Meownager (•ㅅ•)</h2>
         </div>
     )
 }
