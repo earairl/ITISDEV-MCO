8ab2c8c66c015bcb6d1f537f31c47d7e0c5dd2c4
S15AChanK <kole_chan@dlsu.edu.ph>
2025-07-02 13:45:59 +0800

    Update member position to allow 2 values only

--- server/models/Member.js
+++ server/models/Member.js
@@ -5,7 +5,10 @@ const memberSchema = new mongoose.Schema({
     firstName:      {type: String, required: true},
     lastName:       {type: String, required: true},
     college:        {type: String, required: true},
-    position:       {type: String},
+    position:       {
+        type: String,
+        enum: ['member', 'officer'], // only allow these values
+    },
     dateJoined:     {type: Date},
     lastMatchJoined:{type: Date},
     isActive:       {type: Boolean, default: true}
